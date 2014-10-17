69f6585ebf1ceac10b3d0a53be45e48d758e1413
lin <lin@lin-PC>
2014-10-17 14:35:48 +0800

    迁移最终版

---
+++ kull-web/src/main/java/com/kull/web/struts/MonthDSAction.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kull.web.struts;
+
+import java.time.Year;
+
+/**
+ *
+ * @author lin
+ */
+public abstract class MonthDSAction extends DSActionSupport{
+    
+    protected int year,month;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    
+    
+    
+}
